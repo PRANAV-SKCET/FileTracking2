f72a1eeb59f56e0f92d9be82b9fc184d74004a12
PRANAV-SKCET <pranavarulprakash@gmail.com>
2024-08-27 23:30:14 +0530

    commit

--- filetracker/src/components/officeWorking.js
+++ filetracker/src/components/officeWorking.js
@@ -1,10 +1,7 @@
-
-
-
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './context';
-import OfficeNavbar from './officeNavbar'; // Import the OfficeNavbar component
+import OfficeNavbar from './officeNavbar';
 
 export default function OfficeWorking() {
     const { setIsOfficeLoggedIn } = useContext(AuthContext);
